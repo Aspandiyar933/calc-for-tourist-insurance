e7eaef998e06c9c3603b6cadf11820d7deafeebc
Aspandiyar933 <ggg.megadoom.lol@gmail.com>
2024-09-03 20:09:21 +0500

    mvp front

--- front/src/components/calc.tsx
+++ front/src/components/calc.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { format } from "date-fns";
-import { Calendar as CalendarIcon, Info } from "lucide-react";
+import { Calendar as CalendarIcon, Mail, Info } from "lucide-react";
 import { APIResponse, InsuranceOption } from '@/types/calc';
 import { Card } from './ui/card';
 
@@ -240,35 +240,139 @@ export function Calc() {
         <div className="mt-4">
           <h2 className="text-xl font-bold mb-2">Результаты:</h2>
           {results.map((result, index) => (
-            <Card className='w-full max-w-4xl mx-auto p-4 bg-gray-50'>
-              <div className='flex items-center justify-between'>
-                <div className='flex items-center space-x-3'>
-                  <div key={index}>
-                    <div className='font-semibold text-lg'>
-                      <h3 className="text-lg font-semibold">{result.insurance_company.name}</h3>
-                    </div>
-                    {/*}
-                    <div className='flex items-center space-x-8'>
-                      <h4 className="text-md font-semibold mt-2">Варианты страховки:</h4>
-                      {renderInsuranceOptions(result.results)}
-                    </div>
-                    */}
-                    <div className="flex items-center space-x-4">
-                      <span className="text-2xl font-bold">{renderInsuranceOptions(result.results[0])} tg</span>
-                      <a href={result.insurance_company.main_page} target="_blank" rel="noopener noreferrer">
-                        <Button className="bg-blue-900 hover:bg-blue-800 text-white">
-                          Купить онлайн
-                        </Button>
-                      </a>
-                      <Info className="w-5 h-5 text-gray-400" />
-                    </div>
-                  </div>
+            <Card className='w-full max-w-4xl mx-auto p-4 m-4 bg-gray-50'>
+              <div key={index}>
+                <div className='font-semibold text-lg'>
+                  <h3 className="text-sm font-semibold">{result.insurance_company.name}</h3>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <span className="text-1xl font-bold">{renderInsuranceOptions(result.results[0])}</span>
+                  <a href={result.insurance_company.main_page} target="_blank" rel="noopener noreferrer">
+                    <Button className="bg-blue-900 hover:bg-blue-800 text-white">
+                      Купить онлайн
+                    </Button>
+                  </a>
+                  <Info className="w-5 h-5 text-gray-400" />
                 </div>
               </div>
             </Card>
           ))}
         </div>
       )}
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-6xl mx-auto p-6">
+        <Card className="md:row-span-2 p-6 bg-white shadow-md flex flex-col">
+          <h3 className="text-3xl font-bold mb-4">Идеально для визы</h3>
+          <p className="text-gray-600 text-lg mb-6">
+            Мы поможем выбрать страховку для путешествия в любую точку мира.
+          </p>
+          <div className="flex-grow flex items-center justify-center bg-gray-100 rounded-lg">
+            <img src={"cardmst.png"} alt="Travel illustration" className="max-w-full h-auto" />
+          </div>
+        </Card>
+        
+        <Card className="p-6 bg-white shadow-md">
+          <h3 className="text-2xl font-bold mb-3">Выгодно</h3>
+          <p className="text-gray-600">
+            Стоимость страховых полисов может различаться. Мы рассчитаем стоимость в нескольких компаниях, чтобы вы могли выбрать самый выгодный вариант.
+          </p>
+        </Card>
+        
+        <Card className="p-6 bg-white shadow-md">
+          <h3 className="text-2xl font-bold mb-3">Без комиссии</h3>
+          <p className="text-gray-600">
+            Наш сервис полностью бесплатен для пользователей. Полисы предлагаются по ценам страховых компаний без дополнительных наценок.
+          </p>
+        </Card>
+      </div>
+      <div className="max-w-6xl mx-auto p-6">
+      <h1 className="text-4xl font-bold mb-6">Как оформить туристическую страховку</h1>
+      
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-12">
+        <div className="bg-gray-200 rounded-lg h-80 flex items-center justify-center">
+          <img src={"woman.png"} alt="happy woman" className="" />
+        </div>
+        <div>
+          <ul className="space-y-4">
+            <li className="flex items-start">
+              <span className="w-2 h-2 bg-black rounded-full mt-2 mr-2"></span>
+              <span>Укажите страну и даты поездки</span>
+            </li>
+            <li className="flex items-start">
+              <span className="w-2 h-2 bg-black rounded-full mt-2 mr-2"></span>
+              <span>Сравните условия и выберите наилучший вариант</span>
+            </li>
+            <li className="flex items-start">
+              <span className="w-2 h-2 bg-black rounded-full mt-2 mr-2"></span>
+              <span>Заполните анкету на сайте</span>
+            </li>
+            <li className="flex items-start">
+              <span className="w-2 h-2 bg-black rounded-full mt-2 mr-2"></span>
+              <span>Оплатите полис</span>
+            </li>
+            <li className="flex items-start">
+              <span className="w-2 h-2 bg-black rounded-full mt-2 mr-2"></span>
+              <span>Получите полис на электронную почту</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <h2 className="text-3xl font-bold mb-6">Что делать если что-то случилось</h2>
+      
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <Card className="p-6 bg-white shadow-md">
+          <h3 className="text-xl font-bold mb-2">1. Обратитесь к вашему ассистансу</h3>
+          <p className="text-gray-600">Воспользуйтесь контактами в вашем полисе</p>
+        </Card>
+        
+        <Card className="p-6 bg-white shadow-md">
+          <h3 className="text-xl font-bold mb-2">2. Следуйте указаниям оператора</h3>
+          <p className="text-gray-600">Вас проинструктируют по получению помощи и подберут подходящее медицинское учреждение</p>
+        </Card>
+        
+        <Card className="p-6 bg-white shadow-md">
+          <h3 className="text-xl font-bold mb-2">3. Сохраняйте все документы и чеки</h3>
+          <p className="text-gray-600">При возникновении расходов предоставьте их в страховую компанию</p>
+        </Card>
+      </div>
+    </div>
+    <footer className="bg-white shadow-md mt-12 py-8">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row justify-between items-start">
+          <div className="mb-6 md:mb-0">
+            <div className="flex items-center mb-4">
+              <div className="w-12 h-12 rounded-full bg-blue-100 flex items-center justify-center mr-3">
+                <span className="text-blue-500 font-bold text-xl">BO</span>
+              </div>
+              <span className="text-xl font-bold">Best Offer</span>
+            </div>
+            <h3 className="font-semibold mb-2">О проекте:</h3>
+            <p className="text-sm text-gray-600 max-w-xl">
+              BestOffer.kz — это незаменимый финансовый помощник, который помогает
+              вам сделать лучший выбор среди финансовых и страховых продуктов.
+              Платформа предлагает широкий ассортимент предложений от ведущих
+              компаний, обеспечивая удобный поиск, сравнение и оформление
+              продуктов онлайн. С нами вы экономите время и деньги, находя самые
+              выгодные условия. Доверьтесь BestOffer.kz и делайте осознанные
+              финансовые решения с уверенностью.
+            </p>
+          </div>
+          <div className="text-right">
+            <p className="mb-2">Ответим на все ваши вопросы</p>
+            <div className="flex items-center justify-end mb-4">
+              <Mail className="w-5 h-5 text-blue-500 mr-2" />
+              <a href="mailto:boffer.kz@gmail.com" className="text-blue-500 hover:underline">
+                boffer.kz@gmail.com
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="mt-8 pt-8 border-t border-gray-200 text-sm text-gray-500 text-center">
+          © Финансовый маркетплейс BestOffer.kz
+        </div>
+      </div>
+    </footer>
     </div>
   );
 }
